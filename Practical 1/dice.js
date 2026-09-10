4733b6afd606ec25666297daa6d7933abf224638
GunjalArpita <gunjalarpita80@gmail.com>
2026-09-10 19:39:37 +0530

    WEB Framework Practicals

---
+++ Practical 1/dice.js
@@ -0,0 +1,14 @@
+let d1 =Math.floor(Math.random() * 6) + 1
+let d2 =Math.floor(Math.random() * 6) + 1
+let sum = d1+ d2
+
+if(sum % 2 ===0)
+{
+    console.log("YOU WIN !!!");
+    
+}
+else{
+    console.log("YOU LOST !!!!");
+    
+}
+
